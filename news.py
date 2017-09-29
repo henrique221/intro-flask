f8faf854e51b82ccae38d00025130666c435ce79
Rafael Dias <rafael.dias@catho.com>
2017-09-29 16:15:17 -0300

    Iniciando projeto -> Indrodução ao Flask

---
+++ news.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, url_for
+from db import noticias
+
+layout = u"""
+  <html>
+  <head>
+      <title>{title}</title>
+  </head>
+  <body>
+     {body}
+  </body>
+  </html>
+"""
+
+@app.route("/noticias/cadastro/")
+def cadastro():
+    if request.method == "POST":
+        dados_do_form = request.form.to_dict()
+
+
+#Inicialmente não vamos nos preocupar com segurança, csrf ou login, mas nos próximos capítulos desta série iremos evoluir este pequeno app.
